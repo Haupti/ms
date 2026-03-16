f72cd73dd5c42c6e3642bf2f3e27389ef779fde7
redstar <marwin.ack@googlemail.com>
2026-03-16 21:40:15 +0100

    add interpreter flag to print instructions instead of running them

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,49 @@
 #include "../lib/asap/util.hpp"
-#include "vm/vm.hpp"
-#include "parser/parser.hpp"
-#include "ir/compile_ir.hpp"
 #include "instr/compile_to_vm.hpp"
+#include "ir/compile_ir.hpp"
+#include "parser/parser.hpp"
 #include "preprocessor/preprocess.hpp"
+#include "vm/vm.hpp"
 #include <filesystem>
+#include <iostream>
+
+void dump_vm_instructions(const std::vector<VMInstr> &instrs) {
+  for (uint64_t i = 0; i < instrs.size(); ++i) {
+    std::cout << vminstr_to_string(i, instrs[i]) << std::endl;
+  }
+}
 
 using namespace std;
 int main(int argc, char **argv) {
-  if (argc < 2) {
+  bool dump_vm = false;
+  string entrypoint = "";
+  std::vector<std::string> msl_args;
+
+  for (int i = 1; i < argc; ++i) {
+    string arg = argv[i];
+    if (arg == "--dump-vm") {
+      dump_vm = true;
+    } else if (entrypoint == "") {
+      entrypoint = arg;
+    } else {
+      msl_args.push_back(arg);
+    }
+  }
+
+  if (entrypoint == "") {
     fail("expected a entrypoint path as argument");
   }
-  filesystem::path path = string(argv[1]);
-  std::vector<std::string> msl_args;
-  for (int i = 2; i < argc; ++i) {
-    msl_args.push_back(argv[i]);
-  }
+
+  filesystem::path path = entrypoint;
   auto tokens = preprocess(path);
   auto nodes = parse(tokens);
   auto ir = compile_ir(nodes);
   auto instrs = compile_to_vm(ir);
+
+  if (dump_vm) {
+    dump_vm_instructions(instrs);
+    return 0;
+  }
+
   return run(instrs, msl_args);
 }
